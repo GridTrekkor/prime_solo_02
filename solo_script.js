439693882648c66a74723a8e2d1e3ffe639843b0
Disco <disco@Discos-MacBook-Pro.local>
2015-09-10 16:27:59 -0500

    Use array join()

--- solo_script.js
+++ solo_script.js
@@ -24,10 +24,7 @@ for(var i = 0; i < MultiArray.length; i++){
 	MultiArray[i] = calculateSTI(MultiArray[i]);   //// should be passing in an index of MultiArray /////////////////////////////
   //console.log(MultiArray[i]);
   newEl = document.createElement('li');
-  MultiArray[i][1] = " " + MultiArray[i][1];    ////  add spaces before the indices 1,2,3 to clean up presentation
-  MultiArray[i][2] = " " + MultiArray[i][2];
-  MultiArray[i][3] = " " + MultiArray[i][3];
-	newText = document.createTextNode(MultiArray[i]);
+	newText = document.createTextNode(MultiArray[i].join(", "));
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
